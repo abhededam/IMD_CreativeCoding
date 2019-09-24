151cb58cc3f47b518cba530bf28c8f98468a1bbd
Aileen Bo Hededam <aileen.b.hededam@stud.h-da.de>
2019-09-24 17:48:59 +0200

    unessesary

--- CC4/CC4_190612_UebungTesting_Hededam/Aufgabe2-Tests/test/sort-test.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert');
-var sort = require('../modules/sort');
-
-describe('Sorter', function () {
-	it('this code is specifically written to sort a character map from the counter. More frequent characters will be listed first', function (done) {
-		assert.deepEqual(sort({b: 1, a: 12}), [['a', 12], ['b', 1]]);
-		done();
-	});
-});
--- CC4/CC4_190612_UebungTesting_Hededam/email/modules/email-validator.js
+++
@@ -1,5 +0,0 @@
-EMAIL_REGEX = /[a-z0-9._%+-]+@[a-z0-9-]+\.[a-z-]{2}/; // do not use! incomplete!
-
-module.exports = function (candidate) {
-	return EMAIL_REGEX.test(candidate);
-};
