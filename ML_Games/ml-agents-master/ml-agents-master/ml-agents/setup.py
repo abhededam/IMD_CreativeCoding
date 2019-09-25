8b13d59a70692404b1c0cc8aa6869bcea740e44f
Aileen Bo Hededam <aileen.b.hededam@stud.h-da.de>
2019-09-25 09:46:53 +0200

    fix ML agent

--- ML_Games/ml-agents-master/ml-agents-master/ml-agents/setup.py
+++ ML_Games/ml-agents-master/ml-agents-master/ml-agents/setup.py
@@ -30,7 +30,7 @@ setup(
 
     install_requires=[
         'mlagents_envs==0.8.1',
-        'tensorflow>=1.7,<1.8',
+        'tensorflow>=1.12.1',
         'Pillow>=4.2.1',
         'matplotlib',
         'numpy>=1.13.3,<=1.14.5',
